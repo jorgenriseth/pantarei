0c460a59ae81216dbfe4a46d9123eb8179d40bc3
jorgenriseth <jnriseth@gmail.com>
2023-08-25 18:09:20 +0200

    updated import interface

--- pantarei/__init__.py
+++ pantarei/__init__.py
@@ -6,3 +6,11 @@ import pantarei.projectors
 import pantarei.solvers
 import pantarei.timekeeper
 import pantarei.utils
+from pantarei.boundary import *
+from pantarei.computers import *
+from pantarei.fenicsstorage import *
+from pantarei.interpolator import *
+from pantarei.mms import *
+from pantarei.solvers import *
+from pantarei.timekeeper import *
+from pantarei.utils import *

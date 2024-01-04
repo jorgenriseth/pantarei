f390594c9c0639aef962a1875d69c07380610870
jorgenriseth <jnriseth@gmail.com>
2024-01-04 13:39:20 +0100

    added mesh-quality script and some additional utilities

--- pantarei/utils.py
+++ pantarei/utils.py
@@ -45,6 +45,7 @@ def single_logger(logger: logging.Logger, logfunc: str, logstring: str):
     if df.MPI.comm_world.rank == 0:
         getattr(logger, logfunc)(logstring)
 
+
 def rank_logger(logger: logging.Logger, logfunc: str, logstring: str):
     getattr(logger, logfunc)(f"Process {df.MPI.comm_world.rank}: {logstring}")
 
@@ -53,9 +54,122 @@ def mpi_single_process_logger(logger: logging.Logger):
     return lambda logfunc, logstring: single_logger(logger, logfunc, logstring)
 
 
+def print_mesh_quality_info(mesh: df.Mesh) -> dict[str, int | float]:
+    num_cells = mesh.num_cells()
+    num_vertices = mesh.num_vertices()
+    min_diameter = 2 * mesh.rmin()
+    max_diameter = 2 * mesh.rmax()
+
+    print(f"{'Mesh Quality Information':^35}")
+    print("-" * 35)
+    print(f"{'Number of cells':<20} : {num_cells}")
+    print(f"{'Number of vertices':<20} : {num_vertices}")
+    print(f"{'Minimum cell diameter':<20} : {min_diameter:.6f}")
+    print(f"{'Maximum cell diameter':<20} : {max_diameter:.6f}")
+    return {
+        "num_cells": num_cells,
+        "num_vertices": num_vertices,
+        "min_diameter": min_diameter,
+        "max_diameter": max_diameter,
+    }
+
+
 def set_optional(
     argument: Optional[T], classname: Callable[..., T], *args, **kwargs
 ) -> T:
     if argument is None:
         argument = classname(*args, **kwargs)
     return argument
+
+
+def mesh_info(mesh: df.Mesh) -> dict[str, int | float]:
+    num_cells = mesh.num_cells()
+    num_vertices = mesh.num_vertices()
+    min_diameter = 2 * mesh.rmin()
+    max_diameter = 2 * mesh.rmax()
+    return {
+        "num_cells": num_cells,
+        "num_vertices": num_vertices,
+        "min_diameter": min_diameter,
+        "max_diameter": max_diameter,
+    }
+
+
+def print_mesh_info(
+    num_cells: int,
+    num_vertices: int,
+    min_diameter: float,
+    max_diameter: float,
+    decimals: int,
+) -> None:
+    print(f"{'Mesh Quality Information':^35}")
+    print("-" * 35)
+    print(f"{'Number of cells':<20} : {num_cells}")
+    print(f"{'Number of vertices':<20} : {num_vertices}")
+    print(f"{'Minimum cell diameter':<20} : {min_diameter:.{decimals}g}")
+    print(f"{'Maximum cell diameter':<20} : {max_diameter:.{decimals}g}")
+
+
+def as_latex_table(
+    num_cells: int,
+    num_vertices: int,
+    min_diameter: float,
+    max_diameter: float,
+    decimals: int,
+    indentsize: int = 4,
+) -> None:
+    indent = " " * indentsize
+    str_out = (
+        latex_header()
+        + 3 * indent
+        + latex_row(
+            num_cells, num_vertices, min_diameter, max_diameter, decimals
+        )
+        + latex_footer()
+    )
+    return str_out
+
+
+def latex_header(index_header: Optional[str] = None):
+    index_headers = [index_header] * (index_header is not None) + [
+        "# Cells",
+        "# Vertices",
+        "$h_{min}$",
+        "$h_{max}$",
+    ]
+    formatted_index_headers = [r"\textbf{" + x + "}" for x in index_headers]
+    return rf"""
+\begin{{table}}[h!]
+    \centering 
+    \begin{{tabular}}{{lcccc}}
+        \hline
+        {" & ".join(formatted_index_headers)} \\
+        \hline
+"""
+
+
+def latex_row(
+    num_cells: int,
+    num_vertices: int,
+    min_diameter: float,
+    max_diameter: float,
+    decimals: int,
+    index: Optional[str] = None,
+) -> str:
+    str_formatter = [index] * (index is not None) + [
+        str(num_cells),
+        str(num_vertices),
+        f"{min_diameter:.{decimals}f}",
+        f"{max_diameter:.{decimals}f}",
+    ]
+    return " & ".join(str_formatter) + r" \\"
+
+
+def latex_footer():
+    return r"""
+        \hline
+    \end{tabular}
+    \caption{Mesh Information}
+    \label{table: mesh info}
+\end{table}
+"""
